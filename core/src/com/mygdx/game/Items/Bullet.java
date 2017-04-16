3fdc1c3d0b9bb03020f694944102b9bd5094391d
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-04-16 17:26:02 +0200

    remove methods which are extends from MovableEntity

--- core/src/com/mygdx/game/Items/Bullet.java
+++ core/src/com/mygdx/game/Items/Bullet.java
@@ -10,12 +10,12 @@ import com.mygdx.game.Entity.MovableEntity;
  */
 
 public class Bullet extends MovableEntity {
-    private final float SPEED = 800;
     private float alpha = 0.3f;
     private int power;
 
     public Bullet(float x, float y, float rotation) {
         super("bullet", x, y);
+        SPEED = 800;
         width = tex.getWidth();
         height = tex.getHeight();
         sprite.setSize(width, height);
@@ -44,29 +44,5 @@ public class Bullet extends MovableEntity {
 
     public void dispose () {
         tex.dispose();
-    }
-
-    @Override
-    public float toRad(float degrees) {
-        return (degrees * MathUtils.PI) / 180.f;
-    }
-
-    @Override
-    public Vector2 getMovementVector(float rotation) {
-        Vector2 v = new Vector2();
-        float rad = toRad(rotation);
-
-        v.x = MathUtils.sin(rad);
-        v.y = -MathUtils.cos(rad);
-
-        return v;
-    }
-
-    @Override
-    public void forward(float delta) {
-        Vector2 w = getMovementVector(sprite.getRotation());
-
-        position.x -= w.x * SPEED * delta;
-        position.y -= w.y * SPEED * delta;
     }
 }
