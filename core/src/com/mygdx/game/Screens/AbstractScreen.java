ae4f4ba18c927113c635c001c44ac9dc1623cb4f
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-04-16 17:24:08 +0200

    create Screens, and gameButton

---
+++ core/src/com/mygdx/game/Screens/AbstractScreen.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Adrian on 2017-04-16.
+ */
+
+public abstract class AbstractScreen implements Screen {
+}
---
+++ core/src/com/mygdx/game/Service/GameButton.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.Service;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+
+/**
+ * Created by Adrian on 2017-04-16.
+ */
+
+public class GameButton extends Button {
+}
