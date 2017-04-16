1c45a56769019a689b14654e892c493140d97c1d
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-04-16 17:29:07 +0200

    add gameButton

--- core/src/com/mygdx/game/Service/GameButton.java
+++ core/src/com/mygdx/game/Service/GameButton.java
@@ -1,10 +1,19 @@
 package com.mygdx.game.Service;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.Entity.Entity;
 
 /**
  * Created by Adrian on 2017-04-16.
  */
 
-public class GameButton extends Button {
+public class GameButton extends Entity {
+
+    public GameButton(String name, float x, float y) {
+        super(name, x, y);
+    }
+
+    public boolean isTouch(Vector2 click) {
+        return Collision.overlaps(click, sprite);
+    }
 }
